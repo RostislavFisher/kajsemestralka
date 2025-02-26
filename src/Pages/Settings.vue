d395cfceb7e89d27eb43f6285e1ae01153e0207e
Rostyslav <rostislavfisher@gmail.com>
2025-02-26 12:17:27 +0100

    settings page is added

--- src/Pages/Settings.vue
+++ src/Pages/Settings.vue
@@ -1,11 +1,25 @@
 <script>
 export default {
-  name: "Settings"
+  name: "Settings",
+  data() {
+    return {
+      city: localStorage.getItem("city") || "Praha"
+    }
+  },
+  methods: {
+    save() {
+      localStorage.setItem("city", this.city);
+    }
+  }
+
 }
 </script>
 
 <template>
-
+<div>
+<input v-model="city">
+</div>
+  <button v-on:click="save">Save</button>
 </template>
 
 <style scoped>
